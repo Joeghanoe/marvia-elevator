e97d4c9f0efb01fa43bb4a6534655798474d890a
Joeghanoe Bhatti <s1154146@student.windesheim.nl>
2021-06-08 15:07:14 +0200

    removed comments

--- src/components/Elevator/Elevator.tsx
+++ src/components/Elevator/Elevator.tsx
@@ -40,8 +40,6 @@ const Elevator = () => {
         callElevatorFloor(elevatorState.direction, index);
     }
 
-    // console.log(elevatorSequence, elevatorSequence[0] ?? 0, elevatorState.activeFloor);
-
     const callElevatorFloor = (direction: number, index: number) => {
         setElevatorState({...elevatorState, direction: direction ? 1 : 0})
         if(elevatorSequence.includes(index)) return;
@@ -78,33 +76,5 @@ const Elevator = () => {
         />
     </div>
 }
-
-// ElevatorContainer
-//     ElevatorState    
-//         ElevatorOrder
-//         ElevatorPosition
-
-//     ElevatorShaft
-//         Elevator
-//             Callback => Up/Down
-//             IsOpen: boolean
-
-//     ElevatorControls
-//         Callback floor number
-
-
-// let elevator = [0, 3, 4, 5]
-
-// let elevatorInput = 2
-// let input = 'down'
-
-// 0 - 2 = 2           [0]                    [0] 
-// 3 - 2 = 1           [1, 0]                 [0, 1, 2, 3]
-// 4 - 2 = 2           [1, 2, 0]               
-// 5 - 2 = 3           [1, 2, 3, 0]    
-
-// let elevatorOrder = [1, 2, 3, 0];
-// let order = elevator[elevatorOrder[0]];
-// elevatorOrder.shift();
 
 export default Elevator
